8729584bb73752fd6d01f9f105ec2194104ff27b
PhilosopherOK <109789712+PhilosopherOK@users.noreply.github.com>
2024-07-15 17:44:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ function sendItem(item) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(item)
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    }).catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
     });
 }
 
